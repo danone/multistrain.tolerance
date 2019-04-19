f4e1439bf575e68b8c018b7126ac85628a33c368
Julien Tap <Julien.Tap@danone.com>
2019-04-19 19:36:38 +0200

    import and clean samples metadata

--- data-raw/download_tolerance_raw_file.R
+++ data-raw/download_tolerance_raw_file.R
@@ -15,4 +15,5 @@ url3="\"https://danone-my.sharepoint.com/:u:/p/julien_tap/EcHWJcIz909Ppu0fR5cEyK
 
 system(paste("wget",url3, "-O data-raw/msp2018_tolerance/IGC.1661MSP.tsv"), show.output.on.console = FALSE, wait = FALSE)
 
-
+url4="\"https://danone-my.sharepoint.com/:t:/p/julien_tap/EWm7fIWD-zhGovAFyv7lzF8BWP2h-LrIk6rZOmJpC_7z6w?e=9bNUgX&download=1\""
+system(paste("wget",url4, "-O data-raw/mapping_metadata.txt"))
---
+++ data-raw/parse_metadata.R
@@ -0,0 +1,9 @@
+
+metadata=read_tsv("data-raw/mapping_metadata.txt")
+
+metadata %>%
+  select(4,6,7,9,10,11,12) %>%
+  mutate(samplenr = samplenr %>% as.character()) %>%
+  mutate(Subject = Subject %>% as.character()) -> sample_metadata
+
+usethis::use_data(sample_metadata)
