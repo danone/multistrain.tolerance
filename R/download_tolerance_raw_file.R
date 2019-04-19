371a438c9c32dfa49d5f2249116dae208c997ddf
Julien Tap <Julien.Tap@danone.com>
2019-04-19 13:05:50 +0200

    add script to download raw file and make the correspondance with MSP

---
+++ R/download_tolerance_raw_file.R
@@ -0,0 +1,19 @@
+
+# generate link file to OneDrive and download using wget or download.file()
+
+
+url1="\"https://danone-my.sharepoint.com/:u:/p/julien_tap/ERsg8WjaviRFhcAbClUwVRcBWSF7gcANqwCTDcb3i72TmQ?e=qIisWR&download=1\""
+
+system(paste("wget",url1, "-O data-raw/msp2018_tolerance/Lifeseq_non-redundant_augmented-catalog_lite.genes_length.tsv"), show.output.on.console = FALSE)
+
+
+url2="\"https://danone-my.sharepoint.com/:t:/p/julien_tap/ETSGzBPmV41AkNPYCEFAqh4BuZB4EsKQDuSAwatfmJb_vQ?e=BapaHS&download=1\""
+
+system(paste("wget",url2, "-O data-raw/msp2018_tolerance/Lifeseq_non-redundant_augmented-catalog.fasta.clstr"), show.output.on.console = FALSE)
+
+
+url3="\"https://danone-my.sharepoint.com/:u:/p/julien_tap/EcHWJcIz909Ppu0fR5cEyKoBL-vAol3h_Gni8tNDMWSjgA?e=DdCLAf&download=1\""
+
+system(paste("wget",url3, "-O data-raw/msp2018_tolerance/IGC.1661MSP.tsv"), show.output.on.console = FALSE)
+
+
