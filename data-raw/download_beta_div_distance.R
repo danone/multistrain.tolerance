9b515559afa843a05554112fddcf26550e87bdd7
Julien Tap <Julien.Tap@danone.com>
2019-05-23 11:34:17 +0200

    download weighted and unweighted unifrac distance

--- data-raw/download_beta_div_distance.R
+++ data-raw/download_beta_div_distance.R
@@ -1,6 +1,13 @@
 
-UNIFRAC_URL = "https://danone-my.sharepoint.com/:t:/p/julien_tap/EZNaXeo4pOlHgpDejJ5LZdYBrFt2uQrrffDn_qwEGR0Xrg?download=1"
+UWUNIFRAC_URL = "https://danone-my.sharepoint.com/:t:/p/julien_tap/EZUxLx7z8FZMp01pGysk4w4BNosuP7d_iGG_e9BonpxOUg?download=1"
 
 
-system(paste("wget", UWUNIFRAC_URL, "-O inst/distance/weighted_unifrac_dm.txt"), ignore.stdout = TRUE, ignore.stderr = TRUE, wait = FALSE)
+system(paste("wget", UWUNIFRAC_URL, "-O inst/distance/unweighted_unifrac_dm.txt"), ignore.stdout = TRUE, ignore.stderr = TRUE, wait = FALSE)
 
+
+
+WUNIFRAC_URL = "https://danone-my.sharepoint.com/:t:/p/julien_tap/EZNaXeo4pOlHgpDejJ5LZdYBdHX_E-nLS4fIfCEHtkHDGQ?download=1"
+
+
+
+system(paste("wget", WUNIFRAC_URL, "-O inst/distance/weighted_unifrac_dm.txt"), ignore.stdout = TRUE, ignore.stderr = TRUE, wait = FALSE)
